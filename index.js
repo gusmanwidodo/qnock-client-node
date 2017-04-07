a4ac849279ae39e1ac8a4cf396532fc6cb666a36
Gusman Widodo <gusman@codigo.id>
2017-04-07 16:26:07 +0700

    Add dependency request, postRequest method

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-var qnock = function() {
+var request = require("request");
+
+var qnock = {
+
+    host: "",
+    id: "",
+    secret: "",
+    fcm: "",
+    token: "",
+
+    config: function (options) {
+        this.host = options.host;
+        this.id = options.id;
+        this.secret = options.secret;
+        this.fcm = options.fcm;
+
+        // this.getToken();
+    },
+
+    getToken: function () {
+
+        var options = {
+            method: 'POST',
+            url: this.url.token
+        };
+
+        request(options, (error, response, body) => {
+
+            if (error) throw new Error(error);
+
+        result = JSON.parse(body);
+
+        this.token = result.DATA;
+
+        console.log(this.token);
+
+    }).auth(this.id, this.secret, false);
+
+    },
+
+    postRequest: function (options) {
+
+        request(options, (error, response, body) => {
+
+            if (error) throw new Error(error);
+
+        result = JSON.parse(body);
+
+        return result;
+
+    }).auth(this.id, this.secret, false);
+
+    }
+
 }
 
 if (typeof module !== 'undefined' && module.exports)
-  module.exports = qnock;
+    module.exports = qnock;
